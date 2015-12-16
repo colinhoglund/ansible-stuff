f7ac3ed9ea67c526abd1b7046bc55c4def2c6198
Colin Hoglund <colinhoglund@gmail.com>
2015-12-16 00:26:14 -0500

    make ec2.py executable

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+ec2_inv=$(dirname $0)/ec2.py
+
 # pull down newest ec2.py inventory script
-wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $(dirname $0)/ec2.py
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $ec2_inv
+chmod +x $ec2_inv
