a027d05cc329f04eb8dfad367ca55fe10e528ac6
Colin Hoglund <colinhoglund@gmail.com>
2015-12-15 19:20:57 -0500

    updating setup.sh and adding gitignore

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # pull down newest ec2.py inventory script
-wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $(dirname $0)/inventory/ec2.py
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $(dirname $0)/ec2.py
