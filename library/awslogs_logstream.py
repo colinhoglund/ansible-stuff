a017c31954262c4bcfcc9db01e122bab9092343f
Colin Hoglund <colinhoglund@gmail.com>
2016-01-18 00:02:15 -0500

    adding documentation to library/awslogs_logstream.py

--- library/awslogs_logstream.py
+++ library/awslogs_logstream.py
@@ -1,5 +1,100 @@
 #!/usr/bin/python
-''' Ansible module for creating logstreams with the Cloudwatch Logs Agent '''
+''' Ansible module that creates logstreams for the Cloudwatch Logs Agent '''
+
+DOCUMENTATION = '''
+---
+module: awslogs_logstream
+author:
+    - Colin Hoglund (@colinhoglund)
+short_description: Creates logstreams for the Cloudwatch Logs Agent
+description:
+  - This module assumes you already have the Cloudwatch Logs Agent already installed
+    Each logstream is stored in a separate file in the /var/awslogs/etc/config/ directory.
+    (U(http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/CWL_GettingStarted.html)).
+notes:
+  - This module supports check mode.
+options:
+  batch_count:
+    required: false
+    default: 1000
+    description:
+      - Specifies the max number of log events in a batch, up to 10000.
+  batch_size:
+    required: false
+    default: 32768
+    description:
+      - Specifies the max size of log events in a batch, in bytes, up to 1048576 bytes.
+        This size is calculated as the sum of all event messages in UTF-8,
+        plus 26 bytes for each log event.
+  buffer_duration:
+    required: false
+    default: 5000
+    description:
+      - Specifies the time duration for the batching of log events. The minimum value is 5000ms.
+  datetime_format:
+    required: true
+    description:
+      - Specifies how the timestamp is extracted from logs.
+  encoding:
+    required: false
+    default: 'utf_8'
+    description:
+      - Specifies the encoding of the log file so that the file can be read correctly.
+  file:
+    aliases: ['name']
+    required: true
+    description:
+      - Specifies log files that you want to push to CloudWatch Logs.
+  file_fingerprint_lines:
+    required: false
+    default: '1'
+    description:
+      - Specifies the range of lines for identifying a file. The valid values are one
+        number or two dash delimited numbers, such as '1', '2-5'.
+  initial_position:
+    choices: ["start_of_file", "end_of_file"]
+    required: false
+    default: 'start_of_file'
+    description:
+      - Specifies where to start to read data.
+  log_group_name:
+    required: false
+    description:
+      - Specifies the destination log group. The default value is the value of the file argument.
+  log_stream_name:
+    required: false
+    default: '{instance_id}'
+    description:
+      - Specifies the destination log stream. You can use a literal string or
+        predefined variables ({instance_id}, {hostname}, {ip_address}), or
+        combination of both to define a log stream name.
+  multi_line_start_pattern:
+    required: false
+    default: '^[^\s]'
+    description:
+      - Specifies the pattern for identifying the start of a log message.
+  time_zone:
+    choices: ["LOCAL", "UTC"]
+    required: false
+    default: 'LOCAL'
+    description:
+      - Specifies the time zone of log event timestamp.
+'''
+
+EXAMPLES = '''
+- name: Create nginx access logstream
+  awslogs_logstream:
+    file: /var/log/nginx/access.log*
+    datetime_format: '%d/%b/%Y:%H:%M:%S'
+
+- name: Create standard Ubuntu logstreams
+  awslogs_logstream:
+    file: "{{item}}"
+    datetime_format: '%b %d %H:%M:%S'
+  with_items:
+    - /var/log/syslog
+    - /var/log/auth.log
+'''
 
 def main():
     ''' main() '''
@@ -7,16 +102,17 @@ def main():
         argument_spec=dict(
             datetime_format          = dict(required=True, type='str'),
             file                     = dict(required=True, type='str'),
-            batch_count              = dict(type='int'),
-            batch_size               = dict(type='int'),
-            buffer_duration          = dict(type='int'),
-            encoding                 = dict(type='str'),
-            file_fingerprint_lines   = dict(type='int'),
-            initial_position         = dict(choices=['start_of_file', 'end_of_file'], type='str'),
-            log_group_name           = dict(type='str'),
-            log_stream_name          = dict(default='{instance_id}', type='str'),
-            multi_line_start_pattern = dict(type='str'),
-            time_zone                = dict(choices=['LOCAL', 'UTC'], type='str'),
+            batch_count              = dict(required=False, type='int'),
+            batch_size               = dict(required=False, type='int'),
+            buffer_duration          = dict(required=False, type='int'),
+            encoding                 = dict(required=False, type='str'),
+            file_fingerprint_lines   = dict(required=False, type='int'),
+            initial_position         = dict(choices=['start_of_file', 'end_of_file'],
+                                            required=False, type='str'),
+            log_group_name           = dict(required=False, type='str'),
+            log_stream_name          = dict(default='{instance_id}', required=False, type='str'),
+            multi_line_start_pattern = dict(required=False, type='str'),
+            time_zone                = dict(choices=['LOCAL', 'UTC'], required=False, type='str'),
         ),
         supports_check_mode=True
     )
@@ -42,10 +138,11 @@ def main():
             msg='{} does not exist, awslogs may not be installed'.format(logstream_config_dir)
         )
 
-    # do nothing if contents have not changed
+    # no changes if contents have not changed
     if os.path.exists(logstream_fullpath) and logstream_contents == open(logstream_fullpath).read():
         module.exit_json(changed=False)
-    else:
+    # no changes if check_mode enabled
+    elif not module.check_mode:
         # write contents to config file
         update_file = open(logstream_fullpath, 'w')
         update_file.write(logstream_contents)
