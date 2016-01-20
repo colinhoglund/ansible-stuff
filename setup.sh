00cd96034bcf5965003095ae260ee2e87236e982
Colin Hoglund <colinhoglund@gmail.com>
2016-01-19 23:56:33 -0500

    add ec2.ini to setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# pull down newest ec2.py inventory script
-ec2_inv=$(dirname $0)/ec2.py
-wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $ec2_inv
-chmod +x $ec2_inv
+# pull down newest ec2.py inventory script and ec2.ini
+ansible_dir=$(dirname $0)
+ec2_py=${ansible_dir}/ec2.py
+ec2_ini=${ansible_dir}/ec2.ini
+
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py -O $ec2_py
+wget https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini -O $ec2_ini
+chmod +x $ec2_py
